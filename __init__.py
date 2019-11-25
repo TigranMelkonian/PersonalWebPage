4a7da4432d188c82900c34c399c2825412149b60
Peter Dearborn <tigran@spotted.us>
2019-11-25 16:06:29 -0500

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,15 @@
+# __init__.py
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
